8093b41b486705f41467747a18288220e7e5f0c0
Eugene <Eugene>
2019-10-26 14:26:03 -0700

    Initial

---
+++ src/main/java/ek/wiki/util/XmlStreamUtils.java
@@ -0,0 +1,23 @@
+package ek.wiki.util;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.events.XMLEvent;
+
+public class XmlStreamUtils
+{
+
+    public static boolean goToTag(XMLEventReader reader, String tag) throws Exception
+    {
+        while (reader.hasNext())
+        {
+            XMLEvent event = reader.nextEvent();
+            if (event.isStartElement() && event.asStartElement().getName().getLocalPart().equals(tag))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
